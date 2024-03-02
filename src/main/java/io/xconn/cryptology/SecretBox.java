7a89032c1ef250d674133bd79ae8c6bd02ee2903
Muzzammil Shahid <muzzammil@xconn.io>
2024-03-02 14:42:19 +0500

    remove functions to prepend and extract nonce

--- src/main/java/io/xconn/cryptology/SecretBox.java
+++ src/main/java/io/xconn/cryptology/SecretBox.java
@@ -1,7 +1,5 @@
 package io.xconn.cryptology;
 
-import java.security.SecureRandom;
-
 import static io.xconn.cryptology.Util.MAC_SIZE;
 import static io.xconn.cryptology.Util.NONCE_SIZE;
 import static io.xconn.cryptology.Util.SECRET_KEY_LEN;
@@ -29,16 +27,6 @@ public class SecretBox {
 
     public static void boxOpen(byte[] output, byte[] nonce, byte[] ciphertext, byte[] privateKey) {
         Util.decrypt(output, nonce, ciphertext, privateKey);
-    }
-
-    public static void prependNonce(byte[] output, byte[] nonce, byte[] cipherWithoutNonce) {
-        System.arraycopy(nonce, 0, output, 0, nonce.length);
-        System.arraycopy(cipherWithoutNonce, 0, output, nonce.length, cipherWithoutNonce.length);
-    }
-
-    public static void extractNonce(byte[] nonce, byte[] cipher, byte[] cipherWithNonce) {
-        System.arraycopy(cipherWithNonce, 0, nonce, 0, nonce.length);
-        System.arraycopy(cipherWithNonce, nonce.length, cipher, 0, cipherWithNonce.length - nonce.length);
     }
 
     public static byte[] generateSecret() {
--- src/test/java/io/xconn/cryptology/SecretBoxTest.java
+++ src/test/java/io/xconn/cryptology/SecretBoxTest.java
@@ -4,15 +4,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static io.xconn.cryptology.SecretBox.box;
 import static io.xconn.cryptology.SecretBox.boxOpen;
-import static io.xconn.cryptology.SecretBox.extractNonce;
 import static io.xconn.cryptology.SecretBox.generateSecret;
-import static io.xconn.cryptology.SecretBox.prependNonce;
 
 public class SecretBoxTest {
 
@@ -74,23 +70,5 @@ public class SecretBoxTest {
         byte[] encrypted = box(nonce, message, secretKey);
         encrypted[Util.NONCE_SIZE + 1] ^= 0xFF; // Modify the byte next to nonce
         assertThrows(IllegalArgumentException.class, () -> boxOpen(nonce, encrypted, secretKey));
-    }
-
-
-    @Test
-    public void testPrependNonceAndExtractNonce() {
-        byte[] nonce = new byte[]{1, 2, 3, 4, 5};
-        byte[] cipherWithoutNonce = new byte[]{6, 7, 8, 9};
-        byte[] cipherWithNonce = new byte[nonce.length + cipherWithoutNonce.length];
-        prependNonce(cipherWithNonce, nonce, cipherWithoutNonce);
-
-        assertArrayEquals(new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, cipherWithNonce);
-
-        byte[] extractedNonce = new byte[nonce.length];
-        byte[] extractedCipher = new byte[cipherWithoutNonce.length];
-        extractNonce(extractedNonce, extractedCipher, cipherWithNonce);
-
-        assertArrayEquals(nonce, extractedNonce);
-        assertArrayEquals(cipherWithoutNonce, extractedCipher);
     }
 }
