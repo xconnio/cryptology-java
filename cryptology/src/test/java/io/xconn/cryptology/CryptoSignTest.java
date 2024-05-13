610410b8277b08f15f6cb59f686da950ba886a11
Muzzammil Shahid <muzzammil@xconn.io>
2024-05-13 12:34:12 +0500

    Add function to verify Cryptosign challenge

--- cryptology/src/test/java/io/xconn/cryptology/CryptoSignTest.java
+++ cryptology/src/test/java/io/xconn/cryptology/CryptoSignTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static io.xconn.cryptology.CryptoSign.generateKeyPair;
 import static io.xconn.cryptology.CryptoSign.getPublicKey;
 import static io.xconn.cryptology.CryptoSign.sign;
+import static io.xconn.cryptology.CryptoSign.verify;
 
 public class CryptoSignTest {
     private static final String privateKey = "6b19991b461d1073918a25525652c4c913fb28b07142faf1146f6bde228653c5";
@@ -32,14 +34,24 @@ public class CryptoSignTest {
         assertArrayEquals(Hex.decode(CryptoSignTest.publicKey), publicKey);
     }
 
+    String challengeString = "f9d17535fb925e9f674d648cbfc41399";
+    String signatureString = "054932bce44c62d749723f808c2f7ba8b3eb6fe27a2886644317" + "cc95022da5b6211866f36572da9ee783fb229e63d0c76ab050e8aa840a48d8285537ed57f70f";
+
     @Test
     public void testSign() {
-        String challengeString = "f9d17535fb925e9f674d648cbfc41399";
-        String expectedSignedString = "054932bce44c62d749723f808c2f7ba8b3eb6fe27a2886644317" +
-                "cc95022da5b6211866f36572da9ee783fb229e63d0c76ab050e8aa840a48d8285537ed57f70f";
-
         byte[] signedChallenge = sign(Hex.decode(privateKey), Hex.decode(challengeString));
 
-        assertArrayEquals(Hex.decode(expectedSignedString), signedChallenge);
+        assertArrayEquals(Hex.decode(signatureString), signedChallenge);
+    }
+
+    @Test
+    public void testVerify() {
+        byte[] challenge = Hex.decode(challengeString);
+        byte[] signature = Hex.decode(signatureString);
+        byte[] publicKeyBytes = Hex.decode(publicKey);
+
+        boolean verified = verify(publicKeyBytes, challenge, signature);
+
+        assertTrue(verified);
     }
 }
