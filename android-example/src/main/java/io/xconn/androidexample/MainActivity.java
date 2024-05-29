2b4928d7d3581cc73e3d85a7f917b87cce688ba8
Muteeb Ur Rehman <139336141+muteeburrehman@users.noreply.github.com>
2024-05-29 19:41:55 +0500

    Add android example app (#32)
    * Add example app using SecretBox and SealedBox
    
    * remove unnecessary files
    
    * Fix build
    
    * Fix existing files
    
    * minor changes
    
    * Fix themes
    
    * Remove unnecesssary permissions
    
    * Add constants for Shared Preference keys
    
    * Fix backstack
    
    * Add missing EOF
    
    * Add missing EOF
    
    * Remove unnecessary code
    
    * Handle backstack
    
    ---------
    
    Co-authored-by: Muzzammil <muzzammil@xconn.io>

--- android-example/src/main/java/io/xconn/androidexample/MainActivity.java
+++ android-example/src/main/java/io/xconn/androidexample/MainActivity.java
@@ -1,14 +1,125 @@
 package io.xconn.androidexample;
 
+import static io.xconn.androidexample.util.Helpers.bytesToHex;
+import static io.xconn.androidexample.util.Helpers.convertTo32Bytes;
+
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+import io.xconn.cryptology.KeyPair;
+import io.xconn.cryptology.SealedBox;
+import io.xconn.cryptology.SecretBox;
+import io.xconn.androidexample.fragment.CameraFragment;
+import io.xconn.androidexample.fragment.GalleryFragment;
+import io.xconn.androidexample.util.App;
+import io.xconn.androidexample.util.Helpers;
+
+public class MainActivity extends AppCompatActivity implements Helpers.PasswordDialogListener {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        Fragment cameraFragment = new CameraFragment();
+        fragmentManager.beginTransaction().replace(R.id.frameLayout, cameraFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            if (item.getItemId() == R.id.menu_camera) {
+                fragment = new CameraFragment();
+            } else if (item.getItemId() == R.id.menu_gallery) {
+                fragment = new GalleryFragment();
+            }
+
+            if (fragment != null) {
+                Fragment currentFragment = fragmentManager.findFragmentById(R.id.frameLayout);
+
+                // Do nothing if already on the selected fragment
+                if (currentFragment != null && currentFragment.getClass().equals(fragment.getClass())) {
+                    return true;
+                }
+
+                // Clear the backstack
+                fragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+
+                FragmentTransaction transaction = fragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, fragment);
+
+                // Add to backstack if it's GalleryFragment
+                if (fragment instanceof GalleryFragment) {
+                    transaction.addToBackStack(null);
+                }
+
+                transaction.commit();
+                return true;
+            }
+            return false;
+        });
+
+        fragmentManager.addOnBackStackChangedListener(() -> {
+            Fragment currentFragment = fragmentManager.findFragmentById(R.id.frameLayout);
+
+            if (currentFragment instanceof CameraFragment) {
+                bottomNavigationView.setSelectedItemId(R.id.menu_camera);
+            } else if (currentFragment instanceof GalleryFragment) {
+                bottomNavigationView.setSelectedItemId(R.id.menu_gallery);
+            }
+        });
+
+        if (!App.getBoolean(App.PREF_IS_DIALOG_SHOWN)) {
+            Helpers.showPasswordDialog(this, this, false);
+        }
+    }
+
+
+    @Override
+    public boolean onPasswordSubmit(String password) {
+        if (password.isEmpty()) {
+            return false;
+        }
+
+        // Generate key pair
+        KeyPair keyPair = SealedBox.generateKeyPair();
+
+        // Convert public key to hexadecimal string and save it
+        String publicKey = bytesToHex(keyPair.getPublicKey());
+        App.saveString(App.PREF_PUBLIC_KEY, publicKey);
+
+        // Generate nonce and save it
+        byte[] nonce = SecretBox.generateNonce();
+        App.saveString(App.PREF_NONCE, bytesToHex(nonce));
+
+        // Encrypt private key with entered password and save it
+        byte[] encryptedPrivateKey = SecretBox.box(nonce, keyPair.getPrivateKey(),
+                Objects.requireNonNull(convertTo32Bytes(password)));
+        App.saveString(App.PREF_PRIVATE_KEY, bytesToHex(encryptedPrivateKey));
+
+        App.saveBoolean(App.PREF_IS_DIALOG_SHOWN, true);
+        return true;
+    }
+
+    @Override
+    public void onPasswordCancel() {
+
+    }
+
+    @Override
+    public void onDismissed(boolean dismissedAfterSubmit) {
+
     }
 }
---
+++ android-example/src/main/java/io/xconn/androidexample/fragment/GalleryFragment.java
@@ -0,0 +1,266 @@
+package io.xconn.androidexample.fragment;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import io.xconn.androidexample.R;
+import io.xconn.androidexample.util.App;
+import io.xconn.androidexample.util.Helpers;
+import io.xconn.cryptology.SealedBox;
+import io.xconn.cryptology.SecretBox;
+
+public class GalleryFragment extends Fragment implements Helpers.PasswordDialogListener {
+
+    private GridView gridView;
+    private static byte[] privateKey;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
+    private final Handler mainHandler = new Handler(Looper.getMainLooper());
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        gridView = view.findViewById(R.id.gridView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        requireActivity().findViewById(R.id.frameLayout).setFocusableInTouchMode(true);
+        requireActivity().findViewById(R.id.frameLayout).requestFocus();
+        Helpers.showPasswordDialog(requireContext(), this,
+                true);
+    }
+
+    @Override
+    public boolean onPasswordSubmit(String password) {
+        boolean isPrivateKeyDecrypted = decryptPrivateKey(password);
+        if (isPrivateKeyDecrypted) {
+            loadImages();
+            return true;
+        } else {
+            Toast.makeText(requireContext(), "Incorrect password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    @Override
+    public void onPasswordCancel() {
+        if (isCameraFragmentFocused()) {
+            focusOnCameraFragment();
+        }
+        navigateToCameraFragment();
+    }
+
+
+    @Override
+    public void onDismissed(boolean dismissedAfterSubmit) {
+        if (!dismissedAfterSubmit) {
+            if (isCameraFragmentFocused()) {
+                focusOnCameraFragment();
+            }
+            navigateToCameraFragment();
+        }
+    }
+
+
+    private boolean decryptPrivateKey(String password) {
+        String encryptedPrivateKeyHex = App.getString(App.PREF_PRIVATE_KEY);
+        String nonceHex = App.getString(App.PREF_NONCE);
+
+        if (!TextUtils.isEmpty(encryptedPrivateKeyHex) && !TextUtils.isEmpty(nonceHex)) {
+            byte[] encryptedPrivateKey = Helpers.hexToBytes(encryptedPrivateKeyHex);
+            byte[] nonce = Helpers.hexToBytes(nonceHex);
+
+            try {
+                privateKey = SecretBox.boxOpen(nonce, encryptedPrivateKey,
+                        Objects.requireNonNull(Helpers.convertTo32Bytes(password)));
+                return true;
+            } catch (Exception e) {
+                Log.e("IOException", "Error reading or decrypting image data", e);
+                return false;
+            }
+        } else {
+            Toast.makeText(requireContext(), "Private key or nonce not found",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    private boolean isCameraFragmentFocused() {
+        if (isAdded()) {
+            return !requireActivity().findViewById(R.id.menu_camera).isSelected();
+        }
+        return false;
+    }
+
+
+    private void focusOnCameraFragment() {
+        requireActivity().findViewById(R.id.menu_camera).performClick();
+    }
+
+    private void navigateToCameraFragment() {
+        if (isAdded()) {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.frameLayout, new CameraFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
+
+
+    private void loadImages() {
+        File directory = new File(requireContext().getFilesDir(), "cryptology");
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                Toast.makeText(requireContext(), "Failed to create directory",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        List<File> imageFiles = new ArrayList<>();
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    imageFiles.add(file);
+                }
+            }
+        }
+
+        ImageAdapter adapter = new ImageAdapter(
+                requireContext(),
+                imageFiles,
+                executorService,
+                mainHandler);
+        gridView.setAdapter(adapter);
+    }
+
+    private static class ImageAdapter extends BaseAdapter {
+
+        private final List<File> imageFiles;
+        private final LayoutInflater inflater;
+        private final ExecutorService executorService;
+        private final Handler mainHandler;
+
+        ImageAdapter(
+                Context context,
+                List<File> imageFiles,
+                ExecutorService executorService,
+                Handler mainHandler) {
+            this.imageFiles = imageFiles;
+            this.inflater = LayoutInflater.from(context);
+            this.executorService = executorService;
+            this.mainHandler = mainHandler;
+        }
+
+        @Override
+        public int getCount() {
+            return imageFiles.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return imageFiles.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.gallery_item, parent, false);
+                holder = new ViewHolder();
+                holder.imageView = convertView.findViewById(R.id.imageView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.imageView.setImageResource(R.drawable.image_icon);
+
+            File imageFile = imageFiles.get(position);
+            decodeAndDecryptImageDataAsync(imageFile, holder.imageView);
+
+            return convertView;
+        }
+
+        private static class ViewHolder {
+            ImageView imageView;
+        }
+
+        private void decodeAndDecryptImageDataAsync(final File imageFile, final ImageView imageView) {
+            executorService.execute(() -> {
+                Bitmap bitmap = decryptImageData(imageFile);
+                mainHandler.post(() -> {
+                    if (bitmap != null) {
+                        imageView.setImageBitmap(bitmap);
+                    } else {
+                        imageView.setImageResource(R.drawable.broken_image);
+                    }
+                });
+            });
+        }
+
+        private Bitmap decryptImageData(File imageFile) {
+            try (FileInputStream fis = new FileInputStream(imageFile)) {
+                byte[] encryptedData = new byte[(int) imageFile.length()];
+                int bytesRead = fis.read(encryptedData);
+                if (bytesRead == -1) {
+                    Log.e("FileInputStream", "No bytes were read from the file");
+                    return null;
+                }
+
+                byte[] decryptedData = SealedBox.sealOpen(encryptedData, privateKey);
+                return BitmapFactory.decodeByteArray(decryptedData, 0, decryptedData.length);
+            } catch (IOException e) {
+                Log.e("IOException", "Error reading or decrypting image data", e);
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        executorService.shutdown();
+    }
+}
